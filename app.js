06e5b1754f10eaa8db077de3628ac611e611931c
Gabrielle Rogers <gabirogers23@gmail.com>
2022-11-06 22:12:46 -0600

    added skipping buckets for player moves

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 
 //function howManyPlayers
-    //have pop up ask for how many players (1 or 2)
-    //Ask for player/players to input name/names
-    //If one player, put 'computer' for player 2
+//have pop up ask for how many players (1 or 2)
+//Ask for player/players to input name/names
+//If one player, put 'computer' for player 2
 
 //Have the game choose player 1 or player 2 to start
 //display who's turn it is alternating according to the rules
 
 const newBoard = [
     4, 4, 4, 4, 4, 4, 0, /* player 1 */ 4, 4, 4, 4, 4, 4, 0 /* player 2 */
-  ];
+];
 
-  const gameState = {
+const gameState = {
     board: newBoard, // from above
     currentPlayer: 1, // switch to 2 when the player swaps
 
-//connect board to indexes
-    playerOneIndexes: {    
+    //connect board to indexes
+    playerOneIndexes: {
         zero: 0,
         one: 1,
         two: 2,
@@ -25,7 +25,7 @@ const newBoard = [
         five: 5,
     },
 
-    six: 6, //player 1 bucket
+    sixEnd: 6, //player 1 bucket
 
     playerTwoIndexes: {
         seven: 7,
@@ -33,83 +33,134 @@ const newBoard = [
         nine: 9,
         ten: 10,
         eleven: 11,
-        tweleve: 12,
+        twelve: 12,
     },
 
-    thirteen: 13, //player 2 bucket 
+    thirteenEnd: 13, //player 2 bucket 
 
+//needs ending index for the following rules:
+// If the last pip lands in the bucket/score keeper then they get to go again 
+    switchPlayer: function(endIndex) {
+        if (gameState.currentPlayer === 1) {
+            if (endIndex === 6){
+                return;
+            }       
+            gameState.currentPlayer = 2;
+        } else {
+            if (endIndex === 12){
+                return;
+            }
+            gameState.currentPlayer = 1;
+        }
+    },
 
-// event.target.innerText = pipAmount;
+    //select the id to match the keys in the playerIndexes Objects
+    move: function (playerClickKey) {
+        let pitIndex;
+        let copyBoard;
 
+        //checking that a player can only access their side
+        if (gameState.currentPlayer === 1) {
+            pitIndex = gameState.playerOneIndexes[playerClickKey];
+        } else {
+            pitIndex = gameState.playerTwoIndexes[playerClickKey];
+        }
 
-// Pick a space and empty one pip per space until you run out
-//select the id to match the keys in the playerIndexes Objects
-  move: function(playerClickKey) {
-    let pitIndex;
+        if (pitIndex === null || pitIndex === undefined) {
+            return; //the player selected an invalid index
+        }
+        // The div must have pips
+        let pipAmount = gameState.board[pitIndex];
+        if (pipAmount === 0) {
+            return;
+        }
+        //empty selected pit
+        gameState.board[pitIndex] = 0;
+        let startIndex;
+        
+        //cut out index 13 for player 1 & index 6 for player 2
+        if (gameState.currentPlayer === 1) {
+            copyBoard = gameState.board.slice(0,13);
+            startIndex = pitIndex + 1;
+        } else {
+            playerOneSide = gameState.board.slice(0,6); //get array index 0-5
+            playerTwoSide = gameState.board.slice(7); //get array index 7-13
+            copyBoard = playerOneSide.concat(playerTwoSide); //this copyBoard is 1 index short so pitIndex will be where to start adding
+            startIndex = pitIndex;
+        }
+        console.log(copyBoard)
+        //find the last index that a pip went into
+        let endIndex = startIndex + pipAmount;
 
-//checking that a player can only access their side
-    if (gameState.currentPlayer = 1) {
-        pitIndex = gameState.playerOneIndexes[playerClickKey];
-    } else {
-        pitIndex = gameState.playerTwoIndexes[playerClickKey];
-    }
+        // empty one pip per pit 
+        for (let i = startIndex; i < endIndex; i++) {
+            //loop around the array using mod to never get over the length of the array.  Returns the remainder and indexes into the array at that remainder amount.
+            let loop = i % copyBoard.length;
+            copyBoard[loop] += 1;
+        }
 
-    if (pitIndex === null || pitIndex === undefined) {
-        return; //the player selected an invalid index
-    }
-    // The div must have pips
-    let pipAmount = gameState.board[pitIndex];
-    if (pipAmount === 0) {
-        return;
-    }
+          // If a player goes around the board, they cannot put any pips in the opposite players bucket/scoreKeeper
 
-    gameState.board[pitIndex] = 0;
-    
-    for (let i = pitIndex + 1; i <= pipAmount; i++) {
-        gameState.board[i] += 1;
-    }
+        if (gameState.currentPlayer === 1) {
+            copyBoard.push(gameState.board[13]);
+        } else {
+            copyBoard.splice(5, 0, gameState.board[6]);
+        }
+        gameState.board = copyBoard;
+        console.log(copyBoard);
 
-    
-    // If a pip goes into the bucket/score keeper it does not come back out
-    // If a player goes around, they cannot put any pips in the opposite players bucket/scoreKeeper
-    // If the last pip lands in the bucket/score keeper then they get to go again 
-    // If the last pip lands on the players side of the board in an empty space they get that pip and the pips across from it 
+        gameState.switchPlayer(endIndex);
+       
+      
+      
+        // If the last pip lands on the players side of the board in an empty space they get that pip and the pips across from it 
         // Make Array Pairs i.e. (index 0 and index 12) are a pair
-  }
+    }
 
 }
 
 
 
 // function computerPlayer
-    //enter Computer under player 2
-    //use math.random to make it choose a player2side index
-    //call when player 1's turn is done 
-    
+//enter Computer under player 2
+//use math.random to make it choose a player2side index
+//call when player 1's turn is done 
+
 
 
 // function endGame
-    //  call if one side is empty of pips
-    //  Display which player wins ~or~ if it was a draw
-
-// Player 1 side indexes = 0,1,2,3,4,5,
-// Player 1 side Bucket = 6
-
-// Player 2 side indexes = 7,8,9,10,11,12,
-// Player 2 side Bucket =13
+//  call if one side is empty of pips
+//  Display which player wins ~or~ if it was a draw
 
 // function gameReset 
-    //html code reset button
-    // reset's the board
-    // reset's the players names?
+//html code reset button
+// reset's the board
+// reset's the players names?
 
-document.getElementById('playerPits').addEventListener('click', (event)=> {
+document.getElementById('playerPits').addEventListener('click', (event) => {
     //purposely named the HTML ids to be the same as the keys to pass them through to the indexed objects
-gameState.move(event.target.id);
-updateBoard();
+    gameState.move(event.target.id);
+    updateBoard();
 
-}) 
+})
 
 function updateBoard() {
-    
+
+    document.getElementById('zero').innerText = gameState.board[0];
+    document.getElementById('one').innerText = gameState.board[1];
+    document.getElementById('two').innerText = gameState.board[2];
+    document.getElementById('three').innerText = gameState.board[3];
+    document.getElementById('four').innerText = gameState.board[4];
+    document.getElementById('five').innerText = gameState.board[5];
+    document.getElementById('sixEnd').innerText = gameState.board[6];
+    document.getElementById('seven').innerText = gameState.board[7];
+    document.getElementById('eight').innerText = gameState.board[8];
+    document.getElementById('nine').innerText = gameState.board[9];
+    document.getElementById('ten').innerText = gameState.board[10];
+    document.getElementById('eleven').innerText = gameState.board[11];
+    document.getElementById('twelve').innerText = gameState.board[12];
+    document.getElementById('thirteenEnd').innerText = gameState.board[13];
+
 }
+
+updateBoard()
